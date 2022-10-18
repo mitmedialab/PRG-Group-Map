b8b07999b122564e7143ea3d983535e1e2a3ad48
Jeff Freilich <freilich@mit.edu>
2022-10-18 17:51:43 -0400

    Added bio

--- people/jeff.ts
+++ people/jeff.ts
@@ -3,9 +3,9 @@ import { describeYourself } from "../builder";
 describeYourself({
     name: "Jeff Freilich",
     email: "freilich@media.mit.edu ",
-    bio: "",
+    bio: "Jeff has been at MIT for over 13 years working to develop and nurture research ties between industry and academia, as well as developing technical online courses. Previously, he has worked in R&D, product management, marketing and business development roles in industry. Jeff holds a B.Sc. in mechanical engineering from Cornell University and a M.Sc. in mechanical engineering from the Ben-Gurion University of the Negev. He also holds a M.Sc.in management from Boston University.",
     role: "Admin & Finance",
     projects: ["Autonomous Agents", "Doodle Bot", "Computational Action", "Day of AI"],
     skills: ["Project Management"],
-    years: 2021,
+    years: 2007,
 });
