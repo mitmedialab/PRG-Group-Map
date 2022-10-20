afc396bc4a678def2b21107157498e4d1af885bd
Parker Malachowsky <pmalacho@media.mit.edu>
2022-10-20 20:14:27 +0000

    Automated commit from gitpod

--- people/parker.ts
+++ people/parker.ts
@@ -3,9 +3,9 @@ import { describeYourself } from "../builder";
 describeYourself({
     name: "Parker Malachowsky",
     email: "pmalacho@media.mit.edu",
-    bio: "Howdy!!",
+    bio: "Hello",
     role: "Tech Developer",
     projects: ["Sound of AI", "AI Storybook", "Contours 2 Classification", "Feature Detection"],
     skills: ["General AI", "Dev"],
-    years: 2020
+    years: 2020,
 });
