dc1ea55c7051a68aee834902bc13666d98995248
akostrow <akostrow@mit.edu>
2022-10-21 20:22:23 +0000

    Automated commit from gitpod

--- categories/themes/ethicsPolicy.ts
+++ categories/themes/ethicsPolicy.ts
@@ -8,6 +8,9 @@ export default theme({
         "Debating technology and AI with your child": "",
         "AI & Data Privacy": "",
         "AI & Ethics in Middle School": "",
-        "Design Justice": ""
+        "Design Justice": {
+            summary: "The project aims to develop methods for design justice audits in classrooms, in academia broadly, and in the design process in educational institutions and corporations. Our work also extends to creating guidelines to inform how design justice can be leveraged in technology design processes and policy development. Overall, this project audits institutions to understand how they engage in design justice (critique) and suggests ways to achieve design justice in a variety of spaces (solutions). This combines for a focus on both critique and design solutions.",
+            links: [ { text: "homepage", link: "https://www.media.mit.edu/projects/design-justice-auditing/overview/" } ],
+        }
     }
 });
